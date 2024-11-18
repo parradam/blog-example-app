91dbc11eaffdeed442fffd60d5f14a76b321bee0
Adam Parr <adamparr.uk@gmail.com>
2024-11-18 21:22:31 +0000

    Replace FBV with CBV, update templates

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,25 +1,12 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
-def post_list(request):
-    post_list = Post.published.all()
-
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        # If page_number is out of range, get last page
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        # If page_number is not an integer, get first page
-        posts = paginator.page(1)
-
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts}
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
