f44be6f15586eae9c549e8f0cb451b8114772873
Adam Parr <adamparr.uk@gmail.com>
2024-11-19 16:11:41 +0000

    Recommend similar posts using overlapping tags

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -45,13 +46,23 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
+    # Determine list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
