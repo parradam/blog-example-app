22a2adf7598d2e4eb0ba55ac61bbdf1dfbdb9229
Adam Parr <adamparr.uk@gmail.com>
2024-11-21 22:47:07 +0000

    Add trigram extension to Postgres DB, use TrigramSimilarity to find words with three consecutive chars in title

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from .forms import CommentForm, EmailPostForm, SearchForm
@@ -144,15 +144,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query)
+                    similarity=TrigramSimilarity('title', query)
                 )
-                .filter(rank__gte=0.3)
-                .order_by('-rank')
+                .filter(similarity__gt=0.3)
+                .order_by('-similarity')
             )
     
     return render(
