f3460d207c9ab0961f5aba69fa993fd2f7d7bd00
Adam Parr <adamparr.uk@gmail.com>
2024-11-21 22:36:41 +0000

    Add stemming and ranking to results

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from .forms import CommentForm, EmailPostForm, SearchForm
@@ -144,10 +144,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body'),
-                ).filter(search=query)
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                )
+                .filter(search=search_query)
+                .order_by('-rank')
             )
     
     return render(
