0e09e0e210ebc7e58f52b7e33969d31e60dc501b
Adam Parr <adamparr.uk@gmail.com>
2024-11-21 22:28:51 +0000

    Implement basic text search

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,7 +1,7 @@
-from email import message
+from django.contrib.postgres.search import SearchVector
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -132,5 +132,30 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                ).filter(search=query)
+            )
+    
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
